d2e2f20ff4360580c298f5fb90d7c3c8ddea9755
ravinator <36293225+ravinator@users.noreply.github.com>
2018-09-20 09:39:47 +0200

    Basis
    Alle basis dingen, zoals vebinding met css en js, een head met alle nodige info.

---
+++ Index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="CSS/style.css">
+        <script src="JS/js.js"></script>
+        <meta charset="UTF-8">
+        <meta name="description" content="Stagair local spot">
+        <meta name="keywords" content="Technolab, Stagairs, Workshops">
+        <meta name="author" content="Ravi Breugom, Alexander Wallaard, Natascha van Baal">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Technolab Stagairspot</title>
+    </head>
+    <body>
+
+        
+ <footer>&copy; Technolab Leiden</footer>
+    </body>
+</html> 
